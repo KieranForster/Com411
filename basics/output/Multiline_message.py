521df6510a1fc652bf86a20a3e0f47c415a42211
repl.it user <replituser@example.com>
2020-11-04 18:19:35 +0000

    changed files

---
+++ basics/output/Multiline_message.py
@@ -0,0 +1,7 @@
+# Display message to the standard output
+print("system Falure Imminent")
+print("System reboot has been initiated . . .\n")
+print(". . . rebooting sensory system")
+print(". . . rebooting output motors")
+print(". . . rebooting hover engine\n")
+print("System online")
